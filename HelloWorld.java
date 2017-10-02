7a3c2aa26924b8f74b8e3d5906f83b7722bd203b
ripkurrle <ripkurrle@gmail.com>
2017-10-02 16:48:51 +1100

    Removed extra space

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World! Dude, nice Git.\n Wassup?");
+		System.out.println("Hello World! Dude, nice Git.\nWassup?");
 	}
 }

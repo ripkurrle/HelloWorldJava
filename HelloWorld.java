954c2237f5bf5b12767dd603352bf7405216e7ea
ripkurrle <ripkurrle@gmail.com>
2017-10-02 17:11:14 +1100

    Another random line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World! Dude, nice Git.\nWassup?");
-		System.out.println("Second line goes here.\n");
+		System.out.println("Second line goes here.");
 	}
 }

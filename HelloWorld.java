9767ba2273b5d9a8e43015f58022a1af7812306f
Alan Kurrle <ripkurrle@gmail.com>
2017-10-11 15:53:43 +1100

    now in intellij

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-// Test Class to print some strings
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World! Dude, nice Git.\nWassup?");
-		System.out.println("Second line goes here.");
-	}
-}
